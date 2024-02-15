624da527a108b7c634060253f572c897d1c5bcbf
rbpolim <rbpolim@gmail.com>
2024-02-15 18:40:01 -0300

    feat: add local environments

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,15 +1,16 @@
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
+import chalk from "chalk";
 
-const connection = postgres(
-  "postgresql://docker:docker@localhost:5432/pizzashop",
-  { max: 1 }
-);
+import { env } from "../env";
 
+const connection = postgres(env.DATABASE_URL, { max: 1 });
 const db = drizzle(connection);
 
 await migrate(db, { migrationsFolder: "drizzle" });
+
+console.log(chalk.greenBright("Migrations applied successfully!"));
 
 await connection.end();
 
